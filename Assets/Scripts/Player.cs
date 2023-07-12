cd10caa3559097c0e2c85cd45c3379501e06f755
TomFardell <50525775+TomFardell@users.noreply.github.com>
2023-07-12 17:15:37 +0100

    Add roll colors and weapon recoil
    - Colors for while the player is rolling and when roll comes off cooldown
    - Recoil added to the weapon. Only applied if speeds up the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    [SerializeField] GameObject _playerBullet;
+    [SerializeField] private GameObject _playerBullet;
+    [SerializeField] private Color RollColor;
+    [SerializeField] private Color RollReadyColor;
 
     private const float MoveSpeed = 8;
     private const float RollCooldown = 1;
     private const float RollDuration = 0.2f;
     private const float RollSpeed = 16;
     private const float BulletSpeed = 20;
-    private const float ShootCooldown = 0.1f;
+    private const float ShootInterval = 0.05f;
+    private const float WeaponRecoil = 2;
+    private const float RollReadyFlashLength = 0.1f;
 
     private readonly KeyCode[] ShootDirections = { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow };
 
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+    private Color _defaultColor;
 
     private Vector2 _moveDirection;
 
@@ -29,7 +35,10 @@ public class Player : MonoBehaviour {
 
     private void Start() {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _nextRoll = 0;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _defaultColor = _spriteRenderer.color;
+        _nextRoll = -RollCooldown;
     }
 
     private void Update() {
@@ -47,15 +56,27 @@ public class Player : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        _rigidbody.velocity = MoveSpeed * _moveDirection.normalized;
+        // Weapon recoil is applied only to speed up the player. The dot product means firing
+        // diagonally behind is less effective than firing directly behind
+        _rigidbody.velocity = MoveSpeed * _moveDirection.normalized -
+            Mathf.Min(WeaponRecoil * Vector2.Dot(_shootDirection.normalized, _moveDirection.normalized), 0) * _moveDirection.normalized;
 
         if (_startRoll) {
+            // 50 is the number of FixedUpdates per second
             _rollUpdatesRemaining = (int)(RollDuration * 50);
+            _spriteRenderer.color = RollColor;
             _startRoll = false;
         }
+
         if (_rollUpdatesRemaining > 0) {
             _rigidbody.velocity = RollSpeed * _rollDirection.normalized;
             _rollUpdatesRemaining--;
+        } else {
+            if (Time.time > _nextRoll && Time.time < _nextRoll + RollReadyFlashLength) {
+                _spriteRenderer.color = RollReadyColor;
+            } else {
+                _spriteRenderer.color = _defaultColor;
+            }
         }
 
         if (Time.time > _nextShoot)
@@ -63,8 +84,8 @@ public class Player : MonoBehaviour {
                 GameObject bullet = Instantiate(_playerBullet);
                 // The z component ensures bullets appear behind the player
                 bullet.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-                bullet.GetComponent<Rigidbody2D>().velocity = BulletSpeed * _shootDirection;
-                _nextShoot = Time.time + ShootCooldown;
+                bullet.GetComponent<Rigidbody2D>().velocity = BulletSpeed * _shootDirection.normalized;
+                _nextShoot = Time.time + ShootInterval;
             }
     }
 }
